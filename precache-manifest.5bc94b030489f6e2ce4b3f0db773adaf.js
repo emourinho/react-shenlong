5d2ab64893b65c558b3452fe3fb6348a6a352fc1
Everton Mourinho <emourinho@mkmservice.com>
2021-11-26 15:03:33 -0300

    Updates

---
+++ precache-manifest.5bc94b030489f6e2ce4b3f0db773adaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76166662465b9b3e7272c885bcfe7c74",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2e8fa2cb8b4abdc27346",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "52fac52d850f1937ea80",
+    "url": "./static/js/2.9e8f8c43.chunk.js"
+  },
+  {
+    "revision": "110ee2e8f16170fc21371d5f02228173",
+    "url": "./static/js/2.9e8f8c43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e8fa2cb8b4abdc27346",
+    "url": "./static/js/main.b18fe2fd.chunk.js"
+  },
+  {
+    "revision": "8362c850ef2e30e91915",
+    "url": "./static/js/runtime-main.2305042b.js"
+  }
+]);
